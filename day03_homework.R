d4f4ed8108a18c93f69c3dfb3ee9439356bac07c
ayanesekine <24zb1066@student.gs.chiba-u.jp>
2025-06-26 17:01:28 +0900

    note

--- day03_homework.R
+++ day03_homework.R
@@ -25,17 +25,21 @@ answer_0 <- 365 * 5
 # HINT: use 1 ft = 0.3048 m
 # HINT: use 1 lb = 0.4536 kg
 
-155 * 0.3048
-48 * 0.4536
+weight_lb <- 160
+height_ft <- 6
 
-weight_lb <- 47.244
-height_ft <-  21.7728
+160 * 0.4536
+6 * 0.3048
+
+answer_1 <- 160 * 0.4536 / (6 * 0.3048)^2
+
 
 # 2. The following code has an error. Fix it by editing the code.
 # (You don't need to save a separate answer for this one, just fix the code.)
 
 volume_oz <- 130
-volume_ml <- Volume_oz * 29.574
+volume_ml <- volume_oz * 29.574
+
 
 
 
@@ -48,10 +52,14 @@ y <- 3
 z <- x + y
 x <- 5
 
+answer_3 <- 8
+
 # 4. The sqrt() function calculates the square root of a number.
 #
 # Use the sqrt() function to calculate the square root of 200, and
 # save your answer in an object called answer_4.
+
+answer_4 <- sqrt(200)
 
 # 5. The seq() function generates sequences of numbers.
 # Its arguments include "from" (the starting number), "to"
@@ -60,6 +68,8 @@ x <- 5
 #
 # Use seq() make a sequence of even numbers starting at 2 and ending at 100, and
 # save your answer in an object called answer_5
+
+answer_5 <- seq(from = 2, to = 100, by = 2)
 
 # 6. Make a vector of logical values (TRUE and FALSE), and save it to an
 #  object called lgl_vec
@@ -70,6 +80,15 @@ x <- 5
 # What is the type of vector that you obtain? Check with the typeof() function.
 #
 # Save your answer in an object called answer_6
+
+lgl_vec <- c(TRUE, FALSE, TRUE)
+
+chr_vec <- c("kagura", "hero")
+
+conb_vec <- c(lgl_vec, chr_vec)
+
+answer_6 <- typeof(conb_vec)
+
 #
 # HINT: use c() to make vectors
 # HINT: characters are always written with quotation marks
@@ -81,5 +100,7 @@ letters
 # with "a"
 #
 # Save your answer in an object called answer_7
+
+answer_7 <- letters[seq(from = 1, to = 26, by = 2)]
 #
 # HINT: Use `seq()` inside the square brackets
